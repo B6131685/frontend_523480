2868eaab5a5b6c531ae0f206ebe1dd1b0b504c8c
Issarapap <film4777@gmail.com>
2022-06-05 14:43:12 +0700

    show bank payment

--- src/app/customer/ordercustomer/ordercustomer.component.ts
+++ src/app/customer/ordercustomer/ordercustomer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PaymentService } from 'src/app/services/payment.service';
 @Component({
   selector: 'app-ordercustomer',
   templateUrl: './ordercustomer.component.html',
@@ -7,12 +8,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class OrdercustomerComponent implements OnInit {
 
+  payment :{
+    bankName:string,
+    accountBank:string,
+    owner:string
+  } = {bankName:'', accountBank:'', owner:''}
   constructor(
       private  ActivatedRoute:ActivatedRoute, 
-      private Router:Router
+      private Router:Router,
+      private PaymentService:PaymentService,
   ) { }
 
   ngOnInit(): void {
+    this.getPayment();
+  }
+
+  getPayment(){
+    this.PaymentService.getPaymentDB().subscribe(
+      data=>{
+        this.payment = data.data 
+      }
+    )
   }
 
   addItem(){
